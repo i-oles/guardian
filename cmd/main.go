e1955e65b2cab71c041b77f210d6977c69edcc65
Igor Oles <igor.oles@websensa.com>
2023-12-17 00:24:45 +0100

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, world!",
+		})
+	})
+
+	err := r.Run(":8080")
+	if err != nil {
+		return
+	}
+}
